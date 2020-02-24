a6f9e8cc0a86d0d0455f55b6256ca9dbe750d66a
Alexander Raithel <araithel@picturesafe.de>
2020-02-24 12:29:33 +0100

    Implemented getting started sample

--- README.md
+++
@@ -1,2 +0,0 @@
-# picturesafe-search-samples
-Samples for the picturesafe-search open source library.
---
+++ src/main/java/de/picturesafe/search/samples/gettingstarted/GettingStarted.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright 2020 picturesafe media/data/bank GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.picturesafe.search.samples.gettingstarted;
+
+import de.picturesafe.search.elasticsearch.DataChangeProcessingMode;
+import de.picturesafe.search.elasticsearch.SingleIndexElasticsearchService;
+import de.picturesafe.search.elasticsearch.config.DocumentBuilder;
+import de.picturesafe.search.elasticsearch.model.AccountContext;
+import de.picturesafe.search.elasticsearch.model.SearchResult;
+import de.picturesafe.search.expression.ConditionExpression;
+import de.picturesafe.search.expression.Expression;
+import de.picturesafe.search.expression.FulltextExpression;
+import de.picturesafe.search.expression.OperationExpression;
+import de.picturesafe.search.expression.ValueExpression;
+import de.picturesafe.search.parameter.SearchParameter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
+
+@Component
+@ComponentScan
+public class GettingStarted {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(GettingStarted.class);
+
+    @Autowired
+    private SingleIndexElasticsearchService singleIndexElasticsearchService;
+
+    public static void main(String[] args) {
+        final AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(GettingStarted.class);
+        final GettingStarted gettingStarted = ctx.getBean(GettingStarted.class);
+        gettingStarted.run();
+        ctx.close();
+    }
+
+    private void run() {
+        singleIndexElasticsearchService.createIndexWithAlias();
+
+        singleIndexElasticsearchService.addToIndex(DataChangeProcessingMode.BLOCKING,
+                DocumentBuilder.id(1).put("title", "This is a test title").build());
+        singleIndexElasticsearchService.addToIndex(DataChangeProcessingMode.BLOCKING,
+                DocumentBuilder.id(2).put("title", "This is another test title").build());
+        singleIndexElasticsearchService.addToIndex(DataChangeProcessingMode.BLOCKING,
+                DocumentBuilder.id(3).put("title", "This is one more test title").build());
+
+        final Expression expression = OperationExpression.builder()
+                .add(new FulltextExpression("test title"))
+                .add(new ValueExpression("id", ConditionExpression.Comparison.GE, 2))
+                .build();
+
+        final SearchResult searchResult = singleIndexElasticsearchService.search(new AccountContext(), expression, new SearchParameter());
+        LOGGER.info(searchResult.toString());
+
+        singleIndexElasticsearchService.deleteIndexWithAlias();
+    }
+}
