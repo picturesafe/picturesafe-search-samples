616fb2f2e3bb3801c8e7cf7c8bddb0d19e0f1756
Alexander Raithel <araithel@picturesafe.de>
2020-03-23 15:57:22 +0100

    Added MustNotExpression sample

---
+++ src/main/java/de/picturesafe/search/samples/expressions/SearchMustNotExpression.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright 2020 picturesafe media/data/bank GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.picturesafe.search.samples.expressions;
+
+import de.picturesafe.search.elasticsearch.DataChangeProcessingMode;
+import de.picturesafe.search.elasticsearch.SingleIndexElasticsearchService;
+import de.picturesafe.search.elasticsearch.model.DocumentBuilder;
+import de.picturesafe.search.elasticsearch.model.SearchResult;
+import de.picturesafe.search.expression.Expression;
+import de.picturesafe.search.expression.MustNotExpression;
+import de.picturesafe.search.expression.ValueExpression;
+import de.picturesafe.search.parameter.SearchParameter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+@Component
+@ComponentScan
+public class SearchMustNotExpression {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(SearchMustNotExpression.class);
+
+    @Autowired
+    private SingleIndexElasticsearchService singleIndexElasticsearchService;
+
+    public static void main(String[] args) {
+        try (AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(SearchMustNotExpression.class)) {
+            final SearchMustNotExpression searchMustNotExpression = ctx.getBean(SearchMustNotExpression.class);
+            searchMustNotExpression.run();
+        }
+    }
+
+    private void run() {
+        try {
+            singleIndexElasticsearchService.createIndexWithAlias();
+
+            singleIndexElasticsearchService.addToIndex(DataChangeProcessingMode.BLOCKING, Arrays.asList(
+                    DocumentBuilder.id(1).put("keyword", "dog").build(),
+                    DocumentBuilder.id(2).put("keyword", "cat").build(),
+                    DocumentBuilder.id(3).put("keyword", "horse").build(),
+                    DocumentBuilder.id(4).put("keyword", "horse").build()
+            ));
+
+            final Expression expression = new MustNotExpression(new ValueExpression("keyword", "horse"));
+            final SearchResult searchResult = singleIndexElasticsearchService.search(expression, SearchParameter.DEFAULT);
+            LOGGER.info(searchResult.toString());
+        } finally {
+            singleIndexElasticsearchService.deleteIndexWithAlias();
+        }
+    }
+}
