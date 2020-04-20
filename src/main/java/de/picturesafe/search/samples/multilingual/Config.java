0c763fad8a75e927acfe4a1cc592c46586e73bfc
Alexander Raithel <araithel@picturesafe.de>
2020-04-20 11:10:53 +0200

    Added configuration for supported locales

--- src/main/java/de/picturesafe/search/samples/multilingual/Config.java
+++ src/main/java/de/picturesafe/search/samples/multilingual/Config.java
@@ -16,26 +16,47 @@
 
 package de.picturesafe.search.samples.multilingual;
 
+import de.picturesafe.search.elasticsearch.FieldConfigurationProvider;
 import de.picturesafe.search.elasticsearch.config.ElasticsearchType;
 import de.picturesafe.search.elasticsearch.config.FieldConfiguration;
+import de.picturesafe.search.elasticsearch.config.IndexPresetConfiguration;
 import de.picturesafe.search.elasticsearch.config.impl.StandardFieldConfiguration;
+import de.picturesafe.search.elasticsearch.impl.StaticFieldConfigurationProvider;
 import de.picturesafe.search.spring.configuration.DefaultElasticConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Primary;
 
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Locale;
+import java.util.Map;
 
 @Configuration
 @ComponentScan(basePackages = {"de.picturesafe.search.elasticsearch"})
 @Import({DefaultElasticConfiguration.class})
 public class Config {
 
+    // By default, the FieldConfigurationProvider defined in DefaultElasticConfiguration/DefaultIndexConfiguration supports German and English.
+    // If other languages are required, the FieldConfigurationProvider bean must be overwritten:
+    @Bean
+    @Primary
+    FieldConfigurationProvider fieldConfigurationProvider(IndexPresetConfiguration indexPresetConfiguration,
+                                                          List<FieldConfiguration> fieldConfigurations) {
+        final Map<String, List<FieldConfiguration>> fieldConfigurationMap = new HashMap<>();
+        fieldConfigurationMap.put(indexPresetConfiguration.getIndexAlias(), fieldConfigurations);
+        final StaticFieldConfigurationProvider fieldConfigurationProvider = new StaticFieldConfigurationProvider(fieldConfigurationMap);
+        fieldConfigurationProvider.setSupportedLocales(Arrays.asList(Locale.GERMAN, Locale.ENGLISH, Locale.FRENCH));
+        return fieldConfigurationProvider;
+    }
+
     @Bean
     List<FieldConfiguration> fieldConfigurations() {
-        return Arrays.asList(
+        return Collections.singletonList(
                 StandardFieldConfiguration.builder("title", ElasticsearchType.TEXT).multilingual(true).build()
         );
     }
